faec91e5872ebdb9c5284b08d92cc6f688db0f12
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2021-10-12 12:26:34 +0800

    add CRUD methods to controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,53 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :edit, :update, :destroy]
     def index
         @categories = Category.all
     end
+
+    def new
+        @category = Category.new
+    end
+
+    def show
+    end
+
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+            redirect_to @category, notice: 'category was successfully created'
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @category.update(category_params)
+            redirect_to @category
+        else
+            render :edit
+        end
+
+    end
+
+    def destroy
+        if @category.destroy
+            redirect_to categorys_path
+        end
+    end
+
+    private 
+        def set_category
+            @category = Category.find(params[:id])
+        end
+
+        def category_params
+            params.require(:category).permit(:title, :details)
+        end
+
+
 end
